6fe928f29a97d86d09784e0d460d83bc96244284
Chris <cwelde@uci.edu>
2018-01-21 04:01:39 -0800

    Updated program
    Python program now downloads CSV file from URL and saves the CSV file wherever the python program is located

--- csvreader.py
+++ csvreader.py
@@ -1,21 +1,47 @@
-import csv
-import math
-
-with open('angles_UCI_CS.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter = ',')
-
-    stationIDs = {}
-
-    for row in readCSV:
-        stationIDs[row[0]] = row[1]
+from urllib import request #to download from web
+import pandas as pd #to access excel
+import numpy as np #for using cosine on dataframe
 
 
-del stationIDs['station_id']
-
-adict = {k:math.cos(float(v)) for k, v in stationIDs.items()}
-
-print("The format is a dictionary where the key is a Station ID as a string, and the value is the cosine of the angle as float")
-print(adict)
 
 
-        
+hayes_url = 'http://rapid-hub.org/data/angles_UCI_CS.csv'
+
+def download_csvfile(csv_url):
+    response = request.urlopen(csv_url)
+    csv = response.read()
+    
+    csv_str = csv.decode('utf-8')
+    csv_str = csv_str.replace("\r\n", "\n")
+    lines = csv_str
+
+    dest_url = r'angles_UCI_CS.csv'
+
+    fx = open(dest_url, "w")
+
+    for line in lines:
+        fx.write(line)
+
+    fx.close()
+    
+
+download_csvfile(hayes_url)
+
+df = pd.read_csv('angles_UCI_CS.csv')
+
+print('Printing before adding the third column')
+
+print(df)
+
+print('Printing after adding a third column with the computed cosine value')
+
+
+
+df['angle_cosine'] = np.cos(df[' angle_degrees'])
+
+print(df)
+
+
+
+
+
